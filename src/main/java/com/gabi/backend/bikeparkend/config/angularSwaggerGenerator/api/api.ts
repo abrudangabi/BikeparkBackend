73c9c261ed77c514797281c91d8f7d40f4da9273
abrudangabi <abrudan_gabi@yahoo.com>
2019-06-10 11:10:46 +0300

    New Version without ts generated

--- src/main/java/com/gabi/backend/bikeparkend/config/angularSwaggerGenerator/api/api.ts
+++
@@ -1,5 +0,0 @@
-export * from './BikeparkcontrollerApi';
-import { BikeparkcontrollerApi } from './BikeparkcontrollerApi';
-export * from './ConcurscontrollerApi';
-import { ConcurscontrollerApi } from './ConcurscontrollerApi';
-export const APIS = [BikeparkcontrollerApi, ConcurscontrollerApi];
